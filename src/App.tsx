0e835d841a27e4518d932c8d294c7d6ff1ddd597
jiyuujin <jiyuujinunite@gmail.com>
2021-11-27 18:42:39 +0900

    update eslint-config-react

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import HelloWorld from './components/HelloWorld'
 
 const App = () => {
-    return (
-        <>
-            <img alt="React logo" src="@/assets/logo.png" />
-            <HelloWorld msg="Hello React 16.13 + Vite" />
-        </>
-    )
+  return (
+    <>
+      <img alt="React logo" src="@/assets/logo.png" />
+      <HelloWorld msg="Hello React 16.13 + Vite" />
+    </>
+  )
 }
 
 export default App
